7b10f2ef68debfe8c19a97d2895e5219434f4eb3
Mustafa AÇIKGÖZ <99241699+mustafackgz1@users.noreply.github.com>
2022-06-07 00:50:22 +0300

    Update counter.js

--- counter_app/counter.js
+++ counter_app/counter.js
@@ -1,16 +1,3 @@
-// document.getElementById("count-el").innerText = 5;
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// Butonu tanımak:
-
-//  function increment() {
-//      console.log("button clicked")
-//  }
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// BUTONLA BİRLİKTE GİREN KİŞİLERİ SAYMAK VE KAYDETMEK
 let saveEl = document.getElementById("save-el") 
 let countEl = document.getElementById("count-el")
 let count = 0
@@ -22,10 +9,9 @@ function increment(){
 }
 
 function save() {
-    let countStr = count + " - " //bu ve alttaki kod save button a basıldığında numarayı ekrana yazdırır.
+    let countStr = count + " - " //this code rendering the number to the screen when button is clicked.
     saveEl.textContent += countStr;
     countEl.textContent = count
-    //console.log(count)
 }
 
 function reset(){
@@ -35,43 +21,4 @@ function reset(){
     saveEl.textContent = "Previous save: " 
 
 }
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// let username = "Mustafa"
-// let message = "You have tree notifications"
-// let messageToUser = "Hey" + " " +  username + "," + message
-
-// console.log(messageToUser)
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// let name = "Mustafa"
-// let greeting = "Hi, my name is "
-
-// let myGreeting = greeting + name
-
-//console.log(myGreeting)
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-//STRİNG VE NUMBER AYRIMI 
-
-// let var1 = 4
-// let var2 = "10"
-// let var3 = "15"
-// console.log(var1 + var2)
-// console.log(var2 + var1)
-// console.log(var2 + var3)
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// let myname = "mustafa"
-// let greeting = "Welcome back "
-// let welcomeEl = document.getElementById("welcome-el")
-
-// welcomeEl.innerText = (greeting + myname);
-
 
